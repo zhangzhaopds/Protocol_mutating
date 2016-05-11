e53ea861ba97146a664939982561c6181d015599
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-05-11 16:53:22 +0800

    Initial Commit

---
+++ Protocol_mutating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Protocol_mutating
+//
+//  Created by 张昭 on 16/5/11.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
